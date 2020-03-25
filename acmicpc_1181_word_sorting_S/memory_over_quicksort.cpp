1b89105c76bc6d4fdff83ab7de1cc6ccbc380924
Mincheol Kim <62646365+kimmincheol-kor@users.noreply.github.com>
2020-03-25 23:49:38 +0900

    Fail to Solve : Memory Over

---
+++ acmicpc_1181_word_sorting_S/memory_over_quicksort.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int N;
+
+// vecter for store words.
+vector <string> words;
+
+// compare function.
+bool cmp_word(string a, string b)
+{
+	char temp_a, temp_b; // use to compare one character
+
+	// compare size.
+	if (a.size() > b.size())
+		return true;
+	else if (a.size() < b.size())
+		return false;
+	
+	// min between a.size , b.size
+	int min = a.size() > b.size() ? b.size() : a.size();
+
+	// compare characters
+	for (int i = 0; i < min; i++)
+	{
+		temp_a = a[i];
+		temp_b = b[i];
+
+		if (temp_a > temp_b)
+			return true;
+		else if (temp_a < temp_b)
+			return false;
+	}
+}
+
+// quick sort (String)
+void quick_sort(int start, int end)
+{
+	if (start == end) // only one element. -> exit.
+		return;
+
+	string pivot = words[start]; // pivot = first element.
+
+	// vecter for store left, right elements
+	vector<string> left_;
+	vector<string> right_;
+
+	// position of pivot.
+	int position = start;
+
+	// compare and classify. Left, pivot, Right.
+	for (int i = start + 1; i <= end; i++)
+	{
+		if (cmp_word(pivot, words[i]) == true)
+		{
+			left_.push_back(words[i]);
+			position++;
+		}
+		else
+			right_.push_back(words[i]);
+	}
+
+	int iter_l = 0;
+	int iter_r = 0;
+
+	// Sorting
+	for (int i = start; i <= end; i++)
+	{
+		if (i < position)
+			words[i] = left_[iter_l++];
+		else if (i > position)
+			words[i] = right_[iter_r++];
+		else
+			words[i] = pivot;
+	}
+
+	// recursion
+	if (position > start && position < end)
+	{
+		quick_sort(start, position - 1);
+		quick_sort(position + 1, end);
+	}
+	else if (position == end)
+		quick_sort(start, position - 1);
+	else if (position == start)
+		quick_sort(start + 1, end);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+
+	string input_data;
+
+	// user input
+	for (int i = 0; i < N; i++)
+	{
+		cin >> input_data;
+
+		words.push_back(input_data);
+	}
+
+	// sorting
+	quick_sort(0, N - 1);
+
+	// print
+	string pre_word;
+
+	cout << words[0] << "\n";
+	pre_word = words[0];
+
+	for (int i = 0; i < N; i++)
+	{
+		if(pre_word != words[i])
+			cout << words[i] << "\n";
+
+		pre_word = words[i];
+	}
+	
+	return 0;
+}
