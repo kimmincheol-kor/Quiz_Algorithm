5b38b9b96b6d7a564b974c1c065c62e7e471c26d
Mincheol Kim <62646365+kimmincheol-kor@users.noreply.github.com>
2020-04-02 10:29:40 +0900

    Success to Solve : 16ms (Hard, DP)

--- DP/BOJ_1912_DP_successive_sum.cpp
+++ DP/BOJ_1912_DP_successive_sum.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int N;
 
+int max_of_3(int a, int b, int c)
+{
+	return a > b ? (a > c ? a : c) : (b > c ? b : c);
+}
+
 /* BOJ 1912 */
 int main() {
 	ios::sync_with_stdio(false);
@@ -13,73 +18,29 @@ int main() {
 	/* USER INPUT */
 	cin >> N;
 	
-	int* list = new int[N];
-	int* dp = new int[N];
+	int** dp = new int*[N];
+	for (int i = 0; i < N; i++)
+		dp[i] = new int[3];
 
 	for (int i = 0; i < N; i++)
-		cin >> list[i];
+		cin >> dp[i][0];
 
 	/* Processing */
-	dp[0] = list[0];
-	int num_pass = 0;
+	dp[0][1] = dp[0][0];
+	dp[0][2] = -1001;
 
 	for (int i = 1; i < N; i++)
 	{
-		if (list[i] >= 0) // cur value is 0+
-		{
-			if(num_pass == 0)				// successive now.
-				dp[i] = list[i] + dp[i - 1];
-			else							// not successive now.
-			{
-				int temp = 0;
+		dp[i][1] = max_of_3(dp[i][0], dp[i-1][0]+dp[i][0], dp[i-1][1]+dp[i][0]);
+		
+		dp[i][2] = max_of_3(dp[i - 1][0], dp[i - 1][1], dp[i - 1][2]);
 
-				int pass_sum = 0;
-				int max_sum = 0;
-
-				// calculate passed numbers.
-				for (int k = i - num_pass; k <= i; k++)
-				{
-					temp = 0;
-
-					for (int a = k; a <= i; a++)
-						temp += list[a];
-
-					if (k == i - num_pass)
-						pass_sum = temp;
-					if (temp > max_sum)
-						max_sum = temp;
-				}
-
-				// compare.
-				if (pass_sum > 0 && pass_sum + dp[i-1] > max_sum)
-				{
-					dp[i] = dp[i - 1] + pass_sum;
-					num_pass = 0;
-				}
-				else 
-				{
-					if (dp[i - 1] < max_sum)
-					{
-						dp[i] = max_sum;
-						num_pass = 0;
-					}
-					else
-					{
-						dp[i] = dp[i - 1];
-						num_pass += 1;
-					}
-				}
-			}
-		}
-		else // cur value is 0-
-		{
-			dp[i] = dp[i - 1];
-			num_pass += 1;
-		}
+		if (dp[i][2] < dp[i][1])
+			dp[i][2] = -1001;
 	}
 
 	/* PRINT RESULT */
-	cout << dp[N - 1];
+	cout << max_of_3(dp[N-1][0], dp[N - 1][1], dp[N - 1][2]);
 
 	return 0;
 }
