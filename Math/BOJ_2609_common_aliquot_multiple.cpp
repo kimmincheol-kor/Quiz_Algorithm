b84a32cd4ffeab75010ca38b7a85033adef94ffe
Mincheol Kim <62646365+kimmincheol-kor@users.noreply.github.com>
2020-04-05 14:06:03 +0900

    Success to Solve : 156ms (Very Long Time..)

---
+++ Math/BOJ_2609_common_aliquot_multiple.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+/* BOJ 2609 */
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, M;
+
+	int min_num;
+
+	/* USER INPUT */
+	cin >> N >> M;
+
+	min_num = N < M ? N : M;
+	
+	/* Processing */
+	// aliquot
+	for (int i = min_num; i > 0; i--)
+	{
+		if (N % i == 0 && M % i == 0)
+		{
+			cout << i << "\n";
+			break;
+		}
+	}
+
+	// multiple
+	for (int i = 1; i <= N*M; i++)
+	{
+		if (i%N == 0 && i%M == 0)
+		{
+			cout << i;
+			break;
+		}
+	}
+	
+	/* PRINT RESULT */
+	
+	return 0;
+}
