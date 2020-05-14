e6c47f51764d657dcaab9e54505bd4c9a40b5a03
Mincheol Kim <62646365+kimmincheol-kor@users.noreply.github.com>
2020-05-14 15:56:31 +0900

    Update : 16ms -> 12ms

--- DP/BOJ_1912_DP_successive_sum.cpp
+++ DP/BOJ_1912_DP_successive_sum.cpp
@@ -17,8 +17,8 @@ int main() {
 
 	/* USER INPUT */
 	cin >> N;
-	
-	int** dp = new int*[N];
+
+	int** dp = new int* [N];
 	for (int i = 0; i < N; i++)
 		dp[i] = new int[3];
 
@@ -27,20 +27,23 @@ int main() {
 
 	/* Processing */
 	dp[0][1] = dp[0][0];
-	dp[0][2] = -1001;
+
+	int result = dp[0][1];
 
 	for (int i = 1; i < N; i++)
 	{
-		dp[i][1] = max_of_3(dp[i][0], dp[i-1][0]+dp[i][0], dp[i-1][1]+dp[i][0]);
-		
-		dp[i][2] = max_of_3(dp[i - 1][0], dp[i - 1][1], dp[i - 1][2]);
+		if (dp[i - 1][1] > 0)
+			dp[i][1] = dp[i - 1][1] + dp[i][0];
+		else
+			dp[i][1] = dp[i][0];
 
-		if (dp[i][2] < dp[i][1])
-			dp[i][2] = -1001;
+		if (dp[i][1] > result)
+			result = dp[i][1];
 	}
 
+
 	/* PRINT RESULT */
-	cout << max_of_3(dp[N-1][0], dp[N - 1][1], dp[N - 1][2]);
+	cout << result;
 
 	return 0;
 }
