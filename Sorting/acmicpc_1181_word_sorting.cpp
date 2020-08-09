f36afab55cce4bd015b7e7855e326f667551657c
kimmincheol-kor <kimmincheol.dev@gmail.com>
2020-08-09 22:01:23 +0900

    Update to Solve : 44ms (Use C++ STL)

--- Sorting/acmicpc_1181_word_sorting.cpp
+++ Sorting/acmicpc_1181_word_sorting.cpp
@@ -1,157 +1,53 @@
 #include <iostream>
-#include <string>
 #include <vector>
-
-#define LEFT(i) i*2
-#define RIGHT(i) i*2+1
-#define PARENT(i) i/2
+#include <algorithm>
 
 using namespace std;
 
 int N;
 
-vector <string> words;
-
-bool cmp_word(string a, string b)
+bool cmp_str(string str1, string str2)
 {
-	if (a.size() == b.size())
-		return a > b;
+	if (str1.length() != str2.length())
+		return str1.length() < str2.length();
 	else
-		return a.size() > b.size();
+		return str1 < str2;
 }
 
-class Heap_min
-{
-public:
-	vector <string> list;
-	int cnt = 0;
-
-	// Push Function.
-	void push(string new_data)
-	{
-		int cur_position;
-
-		list.push_back(new_data);
-		cnt++;
-
-		cur_position = cnt;
-
-		while (cur_position > 1) // search Last -> First
-		{
-			if (cmp_word(list[PARENT(cur_position)], list[cur_position]) == true) // need swap.
-			{
-				string temp = list[cur_position];
-				list[cur_position] = list[PARENT(cur_position)];
-				list[PARENT(cur_position)] = temp;
-
-				cur_position = PARENT(cur_position);
-			}
-			else // end of search
-				break;
-		}
-	}
-
-	// Pop Function.
-	string pop()
-	{
-		int cur_position = 1;
-
-		// swap
-		string result = list[1];
-		list[1] = list[cnt];
-		list[cnt] = result;
-
-		// pop
-		list.pop_back();
-		cnt--;
-
-		while (1)
-		{
-			if (LEFT(cur_position) <= cnt && RIGHT(cur_position) <= cnt) // exist LEFT, RIGHT
-			{
-				if (cmp_word(list[LEFT(cur_position)], list[RIGHT(cur_position)]) == false) // LEFT < RIGHT
-				{
-					if (cmp_word(list[LEFT(cur_position)], list[cur_position]) == false)
-					{
-						string temp = list[cur_position];
-						list[cur_position] = list[LEFT(cur_position)];
-						list[LEFT(cur_position)] = temp;
-
-						cur_position = LEFT(cur_position);
-					}
-					else
-						break;
-				}
-				else // LEFT > RIGHT
-				{
-					if (cmp_word(list[RIGHT(cur_position)], list[cur_position]) == false)
-					{
-						string temp = list[cur_position];
-						list[cur_position] = list[RIGHT(cur_position)];
-						list[RIGHT(cur_position)] = temp;
-
-						cur_position = RIGHT(cur_position);
-					}
-					else
-						break;
-				}
-
-
-			}
-			else if (LEFT(cur_position) <= cnt) // exist only LEFT
-			{
-				if (cmp_word(list[LEFT(cur_position)], list[cur_position]) == false) // LEFT < RIGHT
-				{
-					string temp = list[cur_position];
-					list[cur_position] = list[LEFT(cur_position)];
-					list[LEFT(cur_position)] = temp;
-				}
-
-				break;
-			}
-			else // none child
-				break;
-		}
-
-		return result;
-	}
-};
-
+/* BOJ 1181 */
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
+	vector<string> v_str;
+	string temp_str;
+
+	/* USER INPUT */
 	cin >> N;
 
-	// make min heap.
-	Heap_min my_heap;
-	my_heap.list.push_back("0"); // element [0] = null.
-
-	string input_data;
-
-	// user input to min heap.
 	for (int i = 0; i < N; i++)
 	{
-		cin >> input_data;
+		cin >> temp_str;
 
-		my_heap.push(input_data);
+		v_str.push_back(temp_str);
 	}
 
-	// print
-	string pre_word = my_heap.pop();
+	/* Processing */
+	sort(v_str.begin(), v_str.end(), cmp_str);
 
-	cout << pre_word << "\n";
+	/* PRINT RESULT */
+	temp_str = v_str[0];
+	cout << v_str[0] << "\n";
 
 	for (int i = 1; i < N; i++)
 	{
-		string temp = my_heap.pop();
-
-		if(pre_word != temp)
-			cout << temp << "\n";
-
-		pre_word = temp;
+		if (temp_str != v_str[i])
+		{
+			temp_str = v_str[i];
+			cout << v_str[i] << "\n";
+		}
 	}
-	
+
 	return 0;
 }
