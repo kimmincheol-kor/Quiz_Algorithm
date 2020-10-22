82537c85f195d01e68512bd27b0506fe1d51a544
kimmincheol-kor <kimmincheol.dev@gmail.com>
2020-10-22 16:26:51 +0900

    Upload PrefixSum Algorithm Code

---
+++ [This_is_Coding_Test]-Quiz/9.Etc/PrefixSum.py
@@ -0,0 +1,14 @@
+n = 5
+data = [10, 20, 30, 40, 50]
+
+# Calculate Prifix Sum
+sum_value = 0
+prefix_sum = [0]
+for i in data:
+    sum_value += i
+    prefix_sum.append(sum_value)
+
+# Get Sum in Range
+left = 3
+right = 4
+print(prefix_sum[right] - prefix_sum[left - 1])
